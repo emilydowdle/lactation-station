1ceb03a264ce559999fd7bfb23f70ec7575ca0dd
Emily Dowdle <emily@editingemily.com>
2016-01-14 19:38:30 -0700

    Add fonts to precompile list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def cache_key_for(model_class, label="")
+    prefix       = model_class.to_s.downcase.pluralize
+    last_updated = model_class.maximum(:updated_at)
+    count        = model_class.count
+    [prefix, label, last_updated, count].join("-")
+  end
 end

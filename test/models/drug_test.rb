32c568c948c4a9a86bc6f96285060b21198e0985
Emily Dowdle <emily@editingemily.com>
2016-01-16 20:00:07 -0700

    Update drug unit test

--- test/models/drug_test.rb
+++ test/models/drug_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class DrugTest < ActiveSupport::TestCase
-  test "starts with query returns multiple " do
-    assert true
+  test "starts with query returns multiple records" do
+    query = Drug.starts_with_query(letter: "D")
+
+    assert_equal 2, query.count
+  end
+
+  test "starts with query returns correct values" do
+    query = Drug.starts_with_query(letter: "D")
+
+    assert_equal Drug.first.id, query[1].id
+    assert_equal Drug.first.name, query[1].name
+  end
+
+  test "when there are no matches starts with query returns no records" do
+    query = Drug.starts_with_query(letter: "A")
+
+    assert_equal 0, query.count
   end
 end
